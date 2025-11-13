3643598532c4b8f658b7f3ccc5be32a44aa31b61
aszxn <aszxnills@gmail.com>
2025-11-13 21:23:02 +0900

    부팅성공

---
+++ src/main/java/com/sinsaflower/server/domain/admin/entity/AdminRole.java
@@ -0,0 +1,11 @@
+package com.sinsaflower.server.domain.admin.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public enum AdminRole {
+    ADMIN,
+    PARTNER
+}
